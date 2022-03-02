447fde434c741029298143064cd7bd04b63f7685
twizeyimana <twizelissa@gmail.com>
2022-03-02 10:53:26 +0200

    mapped transaction action to card widget

--- lib/main.dart
+++ lib/main.dart
@@ -47,14 +47,13 @@ class MyHomePage extends StatelessWidget {
               ),
               elevation: 5,
             ),
-            Card(
-              color: Colors.blue,
-              child: Container(
-                width: 100,
-                child: Text('Chart'),
-              ),
-              elevation: 5,
-            ),
+            Column(
+              children: transactions.map((tx) {
+                return Card(
+                  child: Text(tx.title),
+                );
+              }).toList(),
+            )
           ],
         ));
   }
