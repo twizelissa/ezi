5d23fe0f08af199ec01e00eb3cd54cf69ac3d430
twizeyimana <twizelissa@gmail.com>
2022-03-03 21:16:37 +0200

    added input

--- lib/main.dart
+++ lib/main.dart
@@ -48,11 +48,27 @@ class MyHomePage extends StatelessWidget {
               ),
               elevation: 5,
             ),
-
-            Card(child: Column(children: <Widget>[
-
-            ]),),
-            
+            Card(
+              elevation: 5,
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  
+                  children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Title'),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Amount'),
+                  ),
+                  FlatButton(
+                    child: Text('Add Transaction'),
+                    textColor: Colors.purple,
+                    onPressed: () {},
+                  ),
+                ]),
+              ),
+            ),
             Column(
               children: transactions.map((tx) {
                 return Card(
