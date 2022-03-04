ab2cc253baed2ab2b3aac4d4da3e7a3f2ac7a4d3
twizeyimana <twizelissa@gmail.com>
2022-03-04 08:39:18 +0200

    added newtransaction widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ezi/widget/newTransaction.dart';
 import 'package:flutter/material.dart';
 import './widget/transaction_list.dart';
 
@@ -14,11 +15,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  // String titleInput;
-  // String amountInput;
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,39 +34,7 @@ class MyHomePage extends StatelessWidget {
               ),
               elevation: 5,
             ),
-            Card(
-              elevation: 5,
-              child: Container(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: <Widget>[
-                      TextField(
-                        decoration: InputDecoration(labelText: 'Title'),
-                        controller: titleController,
-                        // onChanged: (val) {
-                        //    titleInput = val;
-                        // },
-                      ),
-                      TextField(
-                        decoration: InputDecoration(labelText: 'Amount'),
-                        controller: amountController,
-                        // onChanged: (val) {
-
-                        //    amountInput = val;
-                        // },
-                      ),
-                      FlatButton(
-                        child: Text('Add Transaction'),
-                        textColor: Colors.purple,
-                        onPressed: () {
-                          print(titleController.text);
-                          print(amountController.text);
-                        },
-                      ),
-                    ]),
-              ),
-            ),
+            NewTransaction(),
             TransactionList(),
           ],
         ));
