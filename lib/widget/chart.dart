3706e0312c531aabf635caaca23406bca9d5642c
twizelissa <twizelissa@gmail.com>
2022-03-30 11:56:27 +0200

    adding chart bar

--- lib/widget/chart.dart
+++ lib/widget/chart.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 class Chart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Card();
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(children: <Widget>[
+
+      ],),
+    );
   }
 }
