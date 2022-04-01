89d848c63d7c332d4a8cfc0444dd500b9e2f5f38
twizelissa <twizelissa@gmail.com>
2022-04-01 19:13:37 +0200

    adding chart

--- lib/widget/chart.dart
+++ lib/widget/chart.dart
@@ -3,18 +3,29 @@ import '../model/transaction.dart';
 import 'package:intl/intl.dart';
 
 class Chart extends StatelessWidget {
-final List <Transaction> recentTransactions; 
+  final List<Transaction> recentTransactions;
 
-Chart(this.recentTransactions);
+  const Chart(this.recentTransactions);
 
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+      double totalSum;
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekDay.day &&
+            recentTransactions[i].date.month == weekDay.month &&
+            recentTransactions[i].date.year == weekDay.year) {
+          totalSum += recentTransactions[i].amount;
+        }
+      }
 
- List<Map<String,Object>> get groupedTransactionValues{
-   return List.generate(7, (index) {
-     final weekDay= DateTime.now().subtract(Duration(days: index));
+      print(DateFormat.E(weekDay));
+      print(totalSum);
 
-    return {'day':DateFormat.E(weekDay),'amount':9.99};
-   });
+      return {'day': DateFormat.E(weekDay), 'amount': totalSum};
+    });
   }
+
   @override
   Widget build(BuildContext context) {
     return Card(
