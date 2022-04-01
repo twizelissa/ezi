5472930ff3a01bc9ba454162c6dbf28dc860d4ef
twizelissa <twizelissa@gmail.com>
2022-04-01 18:02:22 +0200

    adding chart

--- lib/widget/chart.dart
+++ lib/widget/chart.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
+import '../model/transaction.dart';
+import 'package:intl/intl.dart';
 
 class Chart extends StatelessWidget {
+final List <Transaction> recentTransactions; 
+
+Chart(this.recentTransactions);
+
+
+ List<Map<String,Object>> get groupedTransactionValues{
+   return List.generate(7, (index) {
+     final weekDay= DateTime.now().subtract(Duration(days: index));
+
+    return {'day':DateFormat.E(weekDay),'amount':9.99};
+   });
+  }
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 6,
-      margin: EdgeInsets.all(20),
-      child: Row(children: <Widget>[
-
-      ],),
+      margin: const EdgeInsets.all(20),
+      child: Row(
+        children: const <Widget>[],
+      ),
     );
   }
 }
--- lib/widget/newTransaction.dart
+++ lib/widget/newTransaction.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class NewTransaction extends StatefulWidget {
   final Function newTx;
 
-  NewTransaction(this.newTx);
+  const NewTransaction(this.newTx);
 
   @override
   State<NewTransaction> createState() => _NewTransactionState();
@@ -32,12 +32,12 @@ class _NewTransactionState extends State<NewTransaction> {
     return Card(
       elevation: 5,
       child: Container(
-        padding: EdgeInsets.all(10),
+        padding: const EdgeInsets.all(10),
         child: Column(
             crossAxisAlignment: CrossAxisAlignment.end,
             children: <Widget>[
               TextField(
-                decoration: InputDecoration(labelText: 'Title'),
+                decoration: const InputDecoration(labelText: 'Title'),
                 controller: titleController,
                 onSubmitted: (_) => submitData(),
                 // onChanged: (val) {
@@ -45,14 +45,14 @@ class _NewTransactionState extends State<NewTransaction> {
                 // },
               ),
               TextField(
-                decoration: InputDecoration(labelText: 'Amount'),
+                decoration: const InputDecoration(labelText: 'Amount'),
                 controller: amountController,
                 keyboardType: TextInputType.number,
                 onSubmitted: (_) => submitData(),
               ),
               FlatButton(
-                child: Text('Add Transaction'),
-                textColor: Colors.purple,
+                child: const Text('Add Transaction'),
+                textColor: Colors.blue,
                 onPressed: submitData,
               )
             ]),
