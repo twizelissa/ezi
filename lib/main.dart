e118b137abf79cf036faa3c2f5e579c1f1267de6
twizelissa <twizelissa@gmail.com>
2022-04-04 15:49:18 +0200

    adding chart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import './widget/transaction_list.dart';
 import './widget/newTransaction.dart';
 import 'model/transaction.dart';
 import 'package:flutter/material.dart';
+import './widget/chart.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,14 +16,16 @@ class MyApp extends StatelessWidget {
           // accentColor: Colors.black,
           fontFamily: 'Quicksand',
           textTheme: ThemeData.light().textTheme.copyWith(
-              headline6: TextStyle(
+              headline6: const TextStyle(
                   fontFamily: 'OpenSans',
                   fontSize: 18,
                   fontWeight: FontWeight.bold)),
           appBarTheme: AppBarTheme(
-              textTheme: ThemeData.light().textTheme.copyWith(
+              toolbarTextStyle: ThemeData.light().textTheme.copyWith(
                   headline6:
-                      TextStyle(fontFamily: 'Quicksand', fontSize: 20)))),
+                      TextStyle(fontFamily: 'Quicksand', fontSize: 20)).bodyText2, titleTextStyle: ThemeData.light().textTheme.copyWith(
+                  headline6:
+                      TextStyle(fontFamily: 'Quicksand', fontSize: 20)).headline6)),
       home: MyHomePage(),
     );
   }
@@ -35,18 +38,18 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _userTransactions = [
-    Transaction(
-      id: 't1',
-      title: 'Application design',
-      amount: 20000,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Weekly groceries',
-      amount: 1000,
-      date: DateTime.now(),
-    )
+    // Transaction(
+    //   id: 't1',
+    //   title: 'Application design',
+    //   amount: 20000,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: 't2',
+    //   title: 'Weekly groceries',
+    //   amount: 1000,
+    //   date: DateTime.now(),
+    // )
   ];
   void _addNewTransaction(String txTitle, int txAmount) {
     final newTx = Transaction(
@@ -76,17 +79,17 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           'Ezi app',
           style: TextStyle(
               fontFamily: 'OpenSans',
               fontWeight: FontWeight.bold,
               fontSize: 20),
         ),
-        leading: Icon(Icons.menu),
+        leading: const Icon(Icons.menu),
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.add),
+            icon: const Icon(Icons.add),
             onPressed: () => _startAddNewTransaction(context),
           )
         ],
@@ -94,22 +97,15 @@ class _MyHomePageState extends State<MyHomePage> {
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Card(
-              color: Colors.blue,
-              child: Container(
-                width: double.infinity,
-                child: Text('Charting'),
-              ),
-              elevation: 5,
-            ),
+          children: const <Widget>[
+          Chart();
             TransactionList(_userTransactions),
           ],
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
         onPressed: () => _startAddNewTransaction(context),
       ),
     );
